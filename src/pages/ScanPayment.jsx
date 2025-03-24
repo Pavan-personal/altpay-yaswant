3c96fa8dcf09327de8acdf5068a85d342988e71e
03YASWANTH <yaswanthbaratam2580@gmail.com>
2025-03-24 13:40:58 +0530

    pushed

--- src/pages/ScanPayment.jsx
+++ src/pages/ScanPayment.jsx
@@ -3,9 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import { Html5Qrcode } from 'html5-qrcode';
 import { Button } from '@/components/shadcn/button';
 // import { Input } from '@/components/shadcn/input';
-import { GlassPanel, GlassButton, GlassInput } from '@/components/ui/GlassMorphic';
+
 import { Check, QrCode, Send, Smartphone, ArrowLeft, X, Camera } from 'lucide-react';
 import toast, { Toaster } from 'react-hot-toast';
+import { GlassButton, GlassInput, GlassPanel } from '@/components/ui/GlassMorphic';
 
 // Inline implementation of useIsMobile hook
 const useIsMobile = () => {
